1d38dff71b5b1c44323f7cfaf6f9d1a1b230645a
Madongming <elsove812@gmail.com>
2020-08-13 22:26:10 +0800

    add test sample data

--- run.py
+++ run.py
@@ -15,9 +15,11 @@ parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="
 parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
 parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
                     help="Use MQTT over WebSocket")
-parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="ThingShadowEcho",
+parser.add_argument("-id", "--clientId", action="store", dest="clientId",
                     help="Targeted client id")
-parser.add_argument("-pb", "--publish", action="store", dest="publish", default='{"message": "Hello from Python SDK"}',
+parser.add_argument("-pb", "--publish", action="store", dest="publish",
+                    help="Publish payload")
+parser.add_argument("-pf", "--publish-file", action="store", dest="publishFile",
                     help="Publish payload")
 
 args = parser.parse_args()
@@ -29,6 +31,11 @@ port = args.port
 useWebsocket = args.useWebsocket
 clientId = args.clientId
 publish = args.publish
+publishFile = args.publishFile
+
+if not args.clientId:
+    parser.error("clientId is required.")
+    exit(2)
 
 if args.useWebsocket and args.certificatePath and args.privateKeyPath:
     parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
@@ -73,7 +80,19 @@ myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 myMQTTClient.connect()
 time.sleep(2)
 
+publish = args.publish
+publishFile = args.publishFile
 # Publish payload to mqtt.
 while True:
-    myMQTTClient.publish(clientId, publish, 0)
+    if args.publish:
+        myMQTTClient.publish(clientId, publish, 0)
+    elif args.publishFile:
+        with open(publishFile, 'r') as f:
+            for line in f.readlines():
+                myMQTTClient.publish(clientId, line, 0)
+                time.sleep(30)
+    else:
+        parser.error("Publish data is missing. Must be provided with -pb or -pf")
+        exit(2)
+        
     time.sleep(30)
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kill `ps -ef|grep './run.py' | grep -v grep`
+kill `ps -ef|grep './run.py' | grep -v grep | awk '{print $2}'`
